3195af7584486f4a71f4be72522862a32ed3ad44
Amalendu Pandey <amalendupandey56@gmail.com>
2025-09-10 17:27:52 +0530

    robots changed

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
         rules: [
             {
                 userAgent: "*",
-                allow: "/",
+                allow: "/(landing)/",
                 disallow: ["/api/"], // OK if you don’t want APIs crawled
             },
         ],
